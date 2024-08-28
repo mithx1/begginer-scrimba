746fa86897a56e371554679c2c6e5c67c456879c
Julius <71910804+mithx1@users.noreply.github.com>
2024-08-29 00:17:10 +0800

    prepare for create meme api call

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -13,6 +13,8 @@ const MainComponent: React.FC = () => {
   const [templates, setTemplates] = useState<MemeTemplate[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [selectedMeme, setSelectedMeme] = useState<MemeTemplate | null>(null);
+  const [topText, setTopText] = useState<string>("");
+  const [bottomText, setBottomText] = useState<string>("");
 
   useEffect(() => {
     const fetchMemes = async () => {
@@ -31,17 +33,49 @@ const MainComponent: React.FC = () => {
     setSelectedMeme(meme);
   };
 
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const params = {
+      template_id: selectedMeme?.id,
+      text0: topText,
+      text1: bottomText,
+      username: process.env.REACT_APP_IMGFLIP_USERNAME,
+      password: process.env.REACT_APP_IMGFLIP_PASSWORD,
+    };
+  };
+
   return (
     <div className={styles["meme-container"]}>
+      <h1>Pick a meme</h1>
       {error ? (
         <p className={styles["error-message"]}>{error}</p>
       ) : selectedMeme ? (
-        <MemeItem
-          id={selectedMeme.id}
-          name={selectedMeme.name}
-          url={selectedMeme.url}
-          onClick={() => setSelectedMeme(null)}
-        />
+        <form onSubmit={handleSubmit}>
+          <MemeItem
+            id={selectedMeme.id}
+            name={selectedMeme.name}
+            url={selectedMeme.url}
+            onClick={() => setSelectedMeme(null)}
+          />
+          <div>
+            <input
+              type="text"
+              placeholder="Top Text"
+              value={topText}
+              onChange={(e) => setTopText(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Bottom Text"
+              value={bottomText}
+              onChange={(e) => setBottomText(e.target.value)}
+            />
+          </div>
+          <button type="submit">Create Meme</button>
+        </form>
       ) : (
         templates.map((template) => (
           <MemeItem
